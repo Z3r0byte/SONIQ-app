7975ab47b8e3cbb341fb1e0918e191785db6cfb8
Bas van den Boom <basvandenboom3@gmail.com>
2018-11-10 18:11:04 +0100

    Initial commit

---
+++ app/src/main/java/eu/z3r0byteapps/soniq/MainActivity.java
@@ -0,0 +1,13 @@
+package eu.z3r0byteapps.soniq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
