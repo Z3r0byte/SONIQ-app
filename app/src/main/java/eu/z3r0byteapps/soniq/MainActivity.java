237827031102b29d0c1086110e0f4ee868e584ac
Bas van den Boom <basvandenboom3@gmail.com>
2018-11-11 20:53:03 +0100

    Meer animaties toegevoegd

--- app/src/main/java/eu/z3r0byteapps/soniq/MainActivity.java
+++ app/src/main/java/eu/z3r0byteapps/soniq/MainActivity.java
@@ -3,15 +3,15 @@ package eu.z3r0byteapps.soniq;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.CompoundButton;
-import android.widget.ToggleButton;
 
+import br.com.simplepass.loading_button_lib.customViews.CircularProgressButton;
+import br.com.simplepass.loading_button_lib.interfaces.OnAnimationEndListener;
 import me.tankery.app.dynamicsinewave.DynamicSineWaveView;
 
 public class MainActivity extends AppCompatActivity {
 
     DynamicSineWaveView dynamicSineWaveView;
-    ToggleButton listenButton;
+    CircularProgressButton listenButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,24 +19,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listenButton = findViewById(R.id.listen);
-        listenButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        listenButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if (b) {
-                    dynamicSineWaveView.startAnimation();
-                    dynamicSineWaveView.setVisibility(View.VISIBLE);
-                } else {
-                    dynamicSineWaveView.setVisibility(View.INVISIBLE);
-                    dynamicSineWaveView.stopAnimation();
-                }
+            public void onClick(View v) {
+                dynamicSineWaveView.startAnimation();
+                dynamicSineWaveView.setVisibility(View.VISIBLE);
+                listenButton.startAnimation();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Thread.sleep(5000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                dynamicSineWaveView.setVisibility(View.INVISIBLE);
+                                listenButton.revertAnimation(new OnAnimationEndListener() {
+                                    @Override
+                                    public void onAnimationEnd() {
+                                        dynamicSineWaveView.stopAnimation();
+                                    }
+                                });
+                            }
+                        });
+                    }
+                }).start();
             }
         });
 
 
         dynamicSineWaveView = findViewById(R.id.sine_view);
 
-        dynamicSineWaveView.addWave(0.5f, 0.5f, 0, 0, 0); // Fist wave is for the shape of other waves.
-        dynamicSineWaveView.addWave(0.5f, 2f, 0.5f, getResources().getColor(android.R.color.holo_red_dark), 5f);
-        dynamicSineWaveView.addWave(0.3f, 4f, 0.7f, getResources().getColor(android.R.color.holo_blue_dark), 5f);
+        dynamicSineWaveView.addWave(0.5f, 4.0f, 0, 0, 0); // Fist wave is for the shape of other waves.
+        dynamicSineWaveView.addWave(0.5f, 1.1f, 0.5f, getResources().getColor(R.color.colorPrimary), 5f);
+        dynamicSineWaveView.addWave(0.3f, 2f, 0.7f, getResources().getColor(R.color.colorAccent), 5f);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        listenButton.dispose();
     }
 }
