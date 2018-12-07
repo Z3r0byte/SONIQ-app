b383eb481cdf7ec9b7e8b3cfffd291c29e1ad94e
Bas van den Boom <basvandenboom3@gmail.com>
2018-12-07 19:30:33 +0100

    Comments toegevoegd Networking

--- app/src/main/java/eu/z3r0byteapps/soniq/Networking/HttpGet.java
+++ app/src/main/java/eu/z3r0byteapps/soniq/Networking/HttpGet.java
@@ -7,8 +7,10 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class HttpGet {
+    //Via deze class kan een GET request verstuurd worden. Het is niet nodig om hier een instance van aan te maken
     public static OkHttpClient client = new OkHttpClient();
 
+    //GET-request versturen naar de opgegeven URL, returnt het antwoord van de server
     public static String get(String url) throws IOException, NullPointerException {
         Request request = new Request.Builder()
                 .url(url)
--- app/src/main/java/eu/z3r0byteapps/soniq/Networking/HttpPost.java
+++ app/src/main/java/eu/z3r0byteapps/soniq/Networking/HttpPost.java
@@ -11,9 +11,14 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class HttpPost {
+    //Via deze class kan een POST request verstuurd worden. Het is niet nodig om hier een instance van aan te maken
+
+
+    //Definïeren Mediatype header
     public static final MediaType JSON
             = MediaType.parse("application/json; charset=utf-8");
 
+    //Aanmaken client met aangepaste timeouts
     public static OkHttpClient client = new OkHttpClient.Builder()
             .connectTimeout(10, TimeUnit.SECONDS)
             .writeTimeout(10, TimeUnit.SECONDS)
@@ -21,6 +26,8 @@ public class HttpPost {
             .retryOnConnectionFailure(false)
             .build();
 
+
+    // POST-request versturen naar URL met data JSON en search_id uit SEARCH
     public static String post(String url, String json, Search search) throws IOException {
         RequestBody body = RequestBody.create(JSON, json);
         Request request = new Request.Builder()
